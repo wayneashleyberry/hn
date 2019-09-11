8320f06be7a0ec98a67d1fc243d3b36b83c3195a
wayneashleyberry <wayneashleyberry@gmail.com>
2019-09-11 14:09:18 +0100

    cache: wip

--- cmd/hn/main.go
+++ cmd/hn/main.go
@@ -1,37 +1,120 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"os"
+	"path"
 	"time"
 
 	"github.com/dustin/go-humanize"
+	"github.com/patrickmn/go-cache"
 	"github.com/wayneashleyberry/hn/pkg/hackernews"
 	"github.com/wayneashleyberry/hn/pkg/hyperlink"
 	"github.com/wayneashleyberry/truecolor/pkg/color"
 )
 
 func main() {
-	stories, err := hackernews.TopStories()
+	var gobtype []hackernews.Item
+
+	gob.Register(gobtype)
+
+	c := cache.New(5*time.Minute, 10*time.Minute)
+
+	usercache, err := os.UserCacheDir()
+	if err != nil {
+		panic(err)
+	}
+
+	appcachedir := path.Join(usercache, "hn-cli")
+
+	if _, err := os.Stat(appcachedir); os.IsNotExist(err) {
+		err = os.Mkdir(appcachedir, 0700)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	cachefile := path.Join(appcachedir, "cache.gob")
+
+	if _, err := os.Stat(cachefile); err == nil {
+		f, err := os.Open(cachefile)
+		if err == nil {
+			dec := gob.NewDecoder(f)
+
+			var cachedItems map[string]cache.Item
+			err := dec.Decode(&cachedItems)
+			if err == nil {
+				c = cache.NewFrom(5*time.Minute, 10*time.Minute, cachedItems)
+
+				values, present := c.Get("items")
+				if present {
+					items, ok := values.([]hackernews.Item)
+					if ok {
+						fmt.Println("printing from cache")
+						render(items)
+						return
+					}
+				}
+			}
+		}
+		// c = cache.NewFrom(5*time.Minute, 10*time.Minute)
+	}
+
+	ids, err := hackernews.TopStories()
 	if err != nil {
 		return
 	}
 
-	for n, id := range stories {
+	items := []hackernews.Item{}
+
+	for n, id := range ids {
 		if n > 2 {
-			return
+			continue
 		}
 
-		s, err := hackernews.GetItem(id)
+		i, err := hackernews.GetItem(id)
 		if err != nil {
 			return
 		}
 
-		tm := time.Unix(s.Time, 0)
+		items = append(items, i)
+	}
+
+	fmt.Println("printing from source")
+	render(items)
+
+	c.Add("items", items, cache.DefaultExpiration)
+
+	f, err := os.Create(cachefile)
+	if err != nil {
+		panic(err)
+	}
+
+	enc := gob.NewEncoder(f)
+
+	err = enc.Encode(c.Items())
+	if err != nil {
+		panic(err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func render(items []hackernews.Item) {
+	for n, item := range items {
+		tm := time.Unix(item.Time, 0)
 
 		fmt.Printf("%d. ", n+1)
-		hyperlink.Write(os.Stdout, s.URL, s.Title)
+		err := hyperlink.Write(os.Stdout, item.URL, item.Title)
+		if err != nil {
+			continue
+		}
+
 		fmt.Print("\n")
-		color.White().Dim().Printf("   %d points by %s %s\n", s.Score, s.By, humanize.Time(tm))
+		color.White().Dim().Printf("   %d points by %s %s\n", item.Score, item.By, humanize.Time(tm))
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/wayneashleyberry/hn
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/dustin/go-humanize v1.0.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/wayneashleyberry/truecolor v1.0.0
 )
